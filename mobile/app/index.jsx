1954dbe8bbff48960c0cd0b3181b1564676f293b
mAyush24 <ayush2524mishra@gmail.com>
2025-10-15 22:25:26 +0530

    fix

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -2,10 +2,7 @@ import { Text, View, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "yellow",
+    backgroundColor: "purple",
   },
 });
 
